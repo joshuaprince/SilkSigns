981c2383947f271d32a0f0eae5253f0fcb581063
Joshua Prince <joshua@jtprince.com>
2024-07-04 01:27:27 -0700

    Add proper Git version to build recipe

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
 plugins {
     java
     id("io.github.goooler.shadow") version "8.1.8"
+    id("com.palantir.git-version") version "3.1.0"
 }
 
+val gitVersion: groovy.lang.Closure<String> by extra
+
 group = "com.jtprince.silksigns"
-version = "1.0-SNAPSHOT"
+version = gitVersion()
 
 repositories {
     mavenCentral()
@@ -31,13 +34,34 @@ tasks {
 
     processResources {
         filesMatching("plugin.yml") {
-            expand("version" to version)
+            expand("version" to version.toString().trimStart('v'))
         }
+        inputs.property("version", version) // ensure cache is invalidated after version bumps
+    }
+
+    jar {
+        archiveAppendix = "Thin"
     }
 
     shadowJar {
+        archiveAppendix = "Paper"
+        archiveClassifier = "" // Previously "all"
+
         isEnableRelocation = true
         relocationPrefix = "${project.group}.lib"
         minimize()
+    }
+
+    val buildSnapshot = register<Copy>("buildSnapshot") {
+        // Copy the latest artifact from `assemble` task to a consistent place for symlinking into a server.
+        dependsOn(shadowJar)
+        from(shadowJar)
+        into("build/libs")
+        rename { "${rootProject.name}-Paper-SNAPSHOT.jar" }
+    }
+
+    build {
+        dependsOn(shadowJar)
+        dependsOn(buildSnapshot)
     }
 }
